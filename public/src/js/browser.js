32760308f42303f63b48b3e836b7ec57b0fb8516
Zheng-Kun <zheng-kun@foxmail.com>
2018-12-08 22:13:34 +0800

    浏览器版本监测尝试

---
+++ public/src/js/browser.js
@@ -0,0 +1,9 @@
+let browserArr = [{
+  browserName: "360",
+  appName: "Netscape",
+  appVersion: "5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE",
+  userAgent: "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE",
+},{
+  browserName: "QQ",
+  
+}]
